85e1362fc5303b5857eca9d0eb8b16764d2a29ed
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2022-10-05 16:19:14 +0300

    Update Handwritten_character_recog.py

--- Handwritten_character_recog.py
+++ Handwritten_character_recog.py
@@ -31,7 +31,7 @@ train_x, test_x, train_y, test_y = train_test_split(X, y, test_size = 0.2)
 train_x = np.reshape(train_x.values, (train_x.shape[0], 28,28))
 test_x = np.reshape(test_x.values, (test_x.shape[0], 28,28))
 
-
+#further re-shapping
 print("Train data shape: ", train_x.shape)
 print("Test data shape: ", test_x.shape)
 
